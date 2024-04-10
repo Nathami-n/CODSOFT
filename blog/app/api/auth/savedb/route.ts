dc89abfa72ef8f380feb5066f1439cde9ce0db37
Nathan George <georgenathan010@gmail.com>
2024-04-10 09:56:49 +0300

    Created user in db once logged in using KindeAuth

--- blog/app/api/auth/savedb/route.ts
+++ blog/app/api/auth/savedb/route.ts
@@ -1,3 +1,28 @@
+import {getKindeServerSession} from '@kinde-oss/kinde-auth-nextjs/server';
+import prisma from '@/app/utils/prismaClient';
+import {NextResponse} from 'next/server'
+
 export const GET = async () => {
+    const {getUser} = getKindeServerSession();
+    const user = await getUser();
+
+    const dbUser = await prisma.user.findUnique({
+        where: {
+            kindeId: user?.id as string
+        }
+    })
+
+    if(!dbUser) {
+        const newUser = await prisma.user.create({
+            data: {
+                kindeId: user?.id as string,
+                name: user?.given_name as string,
+                email: user?.email as string,
+                imageUrl: user?.picture as string
+            }
+        })
+    }
+
+    return NextResponse.redirect('http://localhost:3000');
     
 }
