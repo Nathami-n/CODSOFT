7e13c0ebfd989d3cbb1e7d48ad3918851e1a0729
Nathan George <georgenathan010@gmail.com>
2024-04-14 22:19:32 +0300

    Fixed unavailable fields in prisma error

--- blog/app/api/auth/savedb/route.ts
+++ blog/app/api/auth/savedb/route.ts
@@ -1,24 +1,25 @@
 import {getKindeServerSession} from '@kinde-oss/kinde-auth-nextjs/server';
 import prisma from '@/app/utils/prismaClient';
 import {NextResponse} from 'next/server'
+import { KindeUser } from '@kinde-oss/kinde-auth-nextjs/types';
 
 export const GET = async () => {
     const {getUser} = getKindeServerSession();
-    const user = await getUser();
+    const user = await getUser() as KindeUser;
 
     const dbUser = await prisma.user.findUnique({
         where: {
-            kindeId: user?.id as string
+            kindeId: user.id as string
         }
     })
 
     if(!dbUser) {
         const newUser = await prisma.user.create({
             data: {
-                kindeId: user?.id as string,
-                name: user?.given_name as string,
-                email: user?.email as string,
-                imageUrl: user?.picture as string
+                kindeId: user.id as string,
+                name: user.given_name as string,
+                email: user.email as string,
+                imageUrl: user.picture as string
             }
         })
     }
