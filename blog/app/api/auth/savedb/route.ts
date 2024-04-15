f7104ef67e815bb7c61c514dead351132f4411a7
Nathan George <georgenathan010@gmail.com>
2024-04-15 09:03:06 +0300

    Trying to fix id error in production

--- blog/app/api/auth/savedb/route.ts
+++ blog/app/api/auth/savedb/route.ts
@@ -5,21 +5,21 @@ import { KindeUser } from '@kinde-oss/kinde-auth-nextjs/types';
 
 export const GET = async () => {
     const {getUser} = getKindeServerSession();
-    const user = await getUser() as KindeUser;
+    const user = await getUser();
 
     const dbUser = await prisma.user.findUnique({
         where: {
-            kindeId: user.id as string
+            kindeId: user?.id as string
         }
     })
 
     if(!dbUser) {
         const newUser = await prisma.user.create({
             data: {
-                kindeId: user.id as string,
-                name: user.given_name as string,
-                email: user.email as string,
-                imageUrl: user.picture as string
+                kindeId: user?.id as string,
+                name: user?.given_name as string,
+                email: user?.email as string,
+                imageUrl: user?.picture as string
             }
         })
     }
