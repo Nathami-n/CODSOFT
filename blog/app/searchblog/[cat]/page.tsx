861aa9f4ea6033f6302b604969a7c15c768a6434
Nathan <georgenathan010@gmail.com>
2024-04-28 20:41:46 +0300

    Added the page functionlity

--- blog/app/searchblog/[cat]/page.tsx
+++ blog/app/searchblog/[cat]/page.tsx
@@ -1,16 +1,52 @@
+import PostCard from "@/app/allblogs/PostCard";
+import prisma from "@/app/utils/prismaClient";
+import { redirect } from "next/navigation";
 
+const fetchPosts = async (category: string) => {
+    const posts = await prisma.post.findMany({
+        where: {
+            category: category,
+            complete: true
+        },
+        select: {
+            id: true,
+            title: true,
+            imageLink: true,
+            body: true,
+            category: true,
+            authorId: true,
+            createdAt: true,
+            author: true
+        }
+    });
 
-const SearchBlog = ({
-    cat
+    return posts;
+};
+
+const SearchBlog = async ({
+    params
 }: {
-    cat: {
-        title: string
+    params: {
+        cat: string
     }
 }) => {
-    const business = await fetchPosts(cat.title);
+    
+    const posts = await fetchPosts(params.cat.toLowerCase());
+    
+    if(posts.length === 0) {
+        redirect('/noblogs');
+    }
+
     return (
-        <div>Hello</div>
+        <div className="px-3">
+        <h1 className="mb-2 font-bold">{params.cat.toUpperCase()}</h1>
+        <div className="flex gap-2 items-center flex-wrap">
+            {posts.map((post) => {
+                return <PostCard key={post.id} post={post} />
+            })}
+            </div>
+            </div>
     )
 };
 
-export default SearchBlog
+export default SearchBlog;
